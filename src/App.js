61235d92676c3445ad7dc872a8ba3bdef9a9ebaa
onemira <mirabulgogi@gmail.com>
2019-03-01 18:03:09 -0500

    saving to github

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import MovieList from './components/MovieList'
+import MovieDetail from './components/MovieDetail'
 
 class App extends Component {
+  state = {
+    movieData: [] //data results
+  }
+
+  componentDidMount() {
+    fetch(
+      'https://api.themoviedb.org/3/movie/now_playing?api_key=<<your key here>>>&language=en-US&page=1'
+    )
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          movieData: data.results
+        })
+      })
+  }
+
   render() {
     ;<Router>
       <Switch>
         <Route exact path="/" component={MovieList} />
-        <Route exact path="/Movie:/:movieId" component={MovieDetail} />
+        <Route exact path="/Movie/:movieId" component={MovieDetail} />
       </Switch>
     </Router>
   }
