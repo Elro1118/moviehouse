69cf6cb48bff7858494124ca9f5396b71ba10b4f
Elro1118 <silvana.rojas8119@gmail.com>
2019-03-01 17:56:56 -0500

    improvingPhase:AppFixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import MovieList from './components/MovieList'
+import MovieDetail from './components/MovieDetail'
 
 class App extends Component {
   render() {
-    ;<Router>
-      <Switch>
-        <Route exact path="/" component={MovieList} />
-        <Route exact path="/Movie:/:movieId" component={MovieDetail} />
-      </Switch>
-    </Router>
+    return (
+      <>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={MovieList} />
+            <Route exact path="/Movie:/:movieId" component={MovieDetail} />
+          </Switch>
+        </Router>
+      </>
+    )
   }
 }
 
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react'
-
+// import axios from 'axios'
 class MovieList extends Component {
+  state = {
+    movies: []
+  }
+
+  componentDidMount() {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?primary_release_year=1989&sort_by=popularity.desc&api_key=fae89ce6616cd4e865bdfb392495d453`
+    )
+      .then(resp => resp.json())
+      .then(data => {
+        console.log({ data })
+        this.setState({
+          movies: data.results
+        })
+      })
+    console.log(this.state.movies)
+  }
+
   render() {
-    return <>hello word</>
+    return (
+      <>
+        <p>hello word</p>
+      </>
+    )
   }
 }
 export default MovieList
